6d99c5388b6f96b31a43252da5f315706a3018eb
Simone Carriero <simonecarriero@gmail.com>
2015-12-16 21:40:19 +0100

    Refactor createActor in Simulation

--- src/main/scala/org/redsimulator/Simulation.scala
+++ src/main/scala/org/redsimulator/Simulation.scala
@@ -1,16 +1,15 @@
 package org.redsimulator
 
 import akka.actor.{Props, ActorSystem}
-import NodeActor.Start
 import org.redsimulator.NodeActor.Start
 
 object Simulation extends App {
   implicit val system = ActorSystem("RedSimulatorSystem")
   val master = system.actorOf(Props(classOf[MasterActor]), "masterActor")
   val numberOfNodes = 10
-  def createActor(i: Integer) = system.actorOf(NodeActor.props(master), s"NodeActor$i")
+  def createActor(i: Int) = system.actorOf(NodeActor.props(master), s"NodeActor$i")
 
   List.range(1, numberOfNodes)
-    .map { createActor(_) }
+    .map {createActor}
     .foreach { _ ! Start }
 }
